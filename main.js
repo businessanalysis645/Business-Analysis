4af271766b349412ee7871d3e6bcede6682950be
businessanalysis645 <141461597+businessanalysis645@users.noreply.github.com>
2025-07-30 18:26:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<a href="https://wa.me/+20 01034927416" class="whatsapp-button" target="_blank" aria-label="تواصل واتساب">
+<a href="https://wa.me/+201034927416" class="whatsapp-button" target="_blank" aria-label="تواصل واتساب">
     <i class="fab fa-whatsapp"></i>
 </a>
-
